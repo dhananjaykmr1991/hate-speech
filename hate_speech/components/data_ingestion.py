b60dca1d8f5f99551bd174bad947e24b6d2a8da6
Dhananjay Kumar <139283219+dhananjaykmr1991@users.noreply.github.com>
2024-04-25 22:51:04 +0530

    data injestion update

--- hate_speech/components/data_ingestion.py
+++ hate_speech/components/data_ingestion.py
@@ -3,6 +3,11 @@ import sys
 from zipfile import ZipFile
 from hate_speech.logger import logging
 from hate_speech.exception import CustomException
-from hate_speech.configuration import gcloud_syncer
+from hate_speech.configuration.gcloud_syncer import GCloudSync 
 from hate_speech.entity.config_entity import DataIngestionConfig
+from hate_speech.entity.artifact_entity import DataIngestionArtifacts
 
+class DataIngestion:
+    def __init__(self, data_ingestion_config : DataIngestionConfig):
+        self.data_ingestion_config = data_ingestion_config
+        self.gcloud = GCloudSync() 
